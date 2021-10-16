ffe036b70932b4daa8862f1826b0d87a210dd5c6
Mehmet Akif Selbi <akifselbii@gmail.com>
2021-10-16 23:58:33 +0300

    16.10.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint 8-1:08:52
     //class : referans tip
